281d69676a513dc124d91a1d316b55c0d9df758d
root <root@masterpe.zen.edu>
2019-05-30 01:01:35 -0400

    Create role and profile

--- Puppetfile
+++ Puppetfile
@@ -1,2 +1,2 @@
 mod 'puppetlabs-stdlib', '6.0.0'
-
+mod 'puppetlabs-ntp', '8.0.0'
---
+++ site/role/manifests/puppetmaster.pp
@@ -0,0 +1,10 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include role::puppetmaster
+class role::puppetmaster {
+  include profile::base
+  include profile::puppetmaster
+}
